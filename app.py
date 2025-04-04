63d79dd4f63f5d1dc1df4de04238061f4960aaf3
Thiago Mazoca <thiago.mzc@hotmail.com>
2025-04-04 16:09:43 -0300

    second commit

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route("/", methods=["GET"])
 def helloWorld():
-	return "Teste da primeira rota no vídeo do Samuel."
+	return "Teste da primeira rota no vídeo do Thiago."
 
 @app.route("/youtube", methods=["GET"])
 def teste():
